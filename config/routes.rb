47777c0e5ebdf15387fa4f2c801a3456f114225b
Matthew Clark <matt@notch8.com>
2017-01-25 08:11:11 -0800

    moves upvote route to nested under questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :upvotes
-  resources :questions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :questions do
+    resources :upvotes
+  end
 end
